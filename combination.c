4b31f20eccc11d460c97acc694d3f7251c9d9c65
Code_with_Manav <154488317+Code-With-Mnv@users.noreply.github.com>
2024-11-15 13:32:38 +0530

    combination.c

--- combination.c
+++ combination.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
-long factorial(int n) {
-      if(n == 0  || n==1)
-      return 1;
+long factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
     long factorial = 1;
+
     for (int i = 2; i <= n; i++)
+    {
         factorial = factorial * i;
+    }
     return factorial;
 }
 
-long nCr(int n, int r) {
+long nCr(int n, int r)
+{
     return factorial(n) / (factorial(r) * factorial(n - r));
 }
 
-int main() {
-    int n = 5, r = 3;
-      printf("%d", nCr(n, r));
+int main()
+{
+    int n, r;
+    printf("Enter the values of n and r: ");
+    scanf("%d %d", &n, &r);
+
+    printf("%d", nCr(n, r));
     return 0;
 }
-
-// This code was contributed by MANAV
