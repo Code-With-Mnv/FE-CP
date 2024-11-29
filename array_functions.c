52691b0f7031ccb5eb291f59b26de46e73faf622
Code_with_Manav <154488317+Code-With-Mnv@users.noreply.github.com>
2024-11-29 09:02:19 +0530

    Update array_functions.c

--- array_functions.c
+++ array_functions.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-
     // generating an array
     int arr_len;
-
     printf("Enter the number of elements in the array: ");
     scanf("%d", &arr_len);
 
     int arr[arr_len];
-
     for (int i = 0; i < arr_len; i++)
     {
         printf("Enter the value: ");
         scanf("%d", &arr[i]);
     }
-
     // average of array
     printf("\nCalculating Average!");
 
     float sum = 0;
     float avg;
-
     for (int i = 0; i < arr_len; i++)
     {
         sum += arr[i];
     }
 
     avg = sum / arr_len;
-    print("Average of the array is: %.1f", avg);
-
+    printf("Average of the array is: %.1f", avg);
     // largest of array
     printf("\nFinding the largest of the array!");
 
     int largest = 0;
-
     for (int i = 0; i < arr_len; i++)
     {
         if (arr[i] > largest)
@@ -45,11 +38,10 @@ void main()
     }
 
     printf("Largest of the array is: %d", largest);
-
     // arrange is descending order
     printf("\nArrangeing in descending order!");
 
-    for (int i = 0; i < arr_len; i++) //[9, 4, 5, 11]
+    for (int i = 0; i < arr_len; i++)
     {
         for (int j = i + 1; j < arr_len; j++)
         {
@@ -60,5 +52,10 @@ void main()
                 arr[j] = temp;
             }
         }
+    }
+
+    for (int i = 0; i < arr_len; i++)
+    {
+        printf("%d ", arr[i]);
     }
 }
